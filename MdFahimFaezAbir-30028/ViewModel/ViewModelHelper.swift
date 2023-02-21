641a1b5995927227ced11b729509a87f7f1050ba
Md Fahim Faez Abir <fahim.abir@bjitacademy.com>
2023-02-21 16:59:58 +0600

    Scorecard done

--- MdFahimFaezAbir-30028/ViewModel/ViewModelHelper.swift
+++ MdFahimFaezAbir-30028/ViewModel/ViewModelHelper.swift
@@ -68,14 +68,16 @@ class ViewModelHelper{
     
     //}
     func setTeamInfo(scorecard: DataClass)-> TeamInfo?{
-        var localTeamScore: Int = 0
-        var visitorTeamScore: Int = 0
-        var localTeamWick: Int = 0
-        var visitorTeamWick: Int = 0
-        var localTeamOver: Double = 0
-        var visitorTeamOver: Double = 0
-        var visitorIninigsExtras: String = ""
-        var localIninigsExtras: String = ""
+        var firstInningsScore: Int = 0
+        var secInningsScore: Int = 0
+        var firstInningsWick: Int = 0
+        var secInningsWick: Int = 0
+        var firstInningsOver: Double = 0
+        var secInningsOver: Double = 0
+        var secIninigsExtras: String = ""
+        var firstIninigsExtras: String = ""
+        var firstInningsTeamName: String = ""
+        var secInningsTeamName: String = ""
         var bye: Int = 0
         var legB: Int = 0
         var penalty: Int = 0
@@ -85,7 +87,7 @@ class ViewModelHelper{
         guard let scoreCard = scorecard.scoreboards else {return nil}
         for score in scoreCard{
             if score.type == "extra"{
-                if score.teamID == scorecard.localteam?.id{
+                if score.scoreboard == "S1"{
                     bye = score.bye ?? 0
                     legB = score.legBye ?? 0
                     penalty = score.penalty ?? 0
@@ -93,8 +95,8 @@ class ViewModelHelper{
                     noBall = score.noballBalls ?? 0
                     wide = score.wide ?? 0
                     let totalExtra = bye + legB + penalty + noballRuns + noBall + wide
-                    localIninigsExtras = String(totalExtra) + ",b " + String(bye) + ",lb " + String(legB) + ",w " + String(wide) + ",nb " + String(noBall) + ",p " + String(penalty)
-                }else if score.teamID == scorecard.visitorteam?.id{
+                    firstIninigsExtras = String(totalExtra) + ",b " + String(bye) + ",lb " + String(legB) + ",w " + String(wide) + ",nb " + String(noBall) + ",p " + String(penalty)
+                }else if score.scoreboard == "S2"{
                     bye = score.bye ?? 0
                     legB = score.legBye ?? 0
                     penalty = score.penalty ?? 0
@@ -102,34 +104,90 @@ class ViewModelHelper{
                     noBall = score.noballBalls ?? 0
                     wide = score.wide ?? 0
                     let totalExtra = bye + legB + penalty + noballRuns + noBall + wide
-                    visitorIninigsExtras = String(totalExtra) + ",b " + String(bye) + ",lb " + String(legB) + ",w " + String(wide) + ",nb " + String(noBall) + ",p " + String(penalty)
+                    secIninigsExtras = String(totalExtra) + ",b " + String(bye) + ",lb " + String(legB) + ",w " + String(wide) + ",nb " + String(noBall) + ",p " + String(penalty)
                 }
             }else if score.type == "total"{
-                if score.teamID == scorecard.localteam?.id {
-                    localTeamScore = score.total ?? 0
-                    localTeamWick = score.wickets ?? 0
-                    localTeamOver = score.overs ?? 0.0
-                }else if score.teamID == scorecard.visitorteam?.id{
-                    visitorTeamScore = score.total ?? 0
-                    visitorTeamOver = score.overs ?? 0.0
-                    visitorTeamWick = score.wickets ?? 0
+                if score.scoreboard == "S1"{
+                    if score.teamID == scorecard.localteamID{
+                        firstInningsTeamName = scorecard.localteam?.name ?? ""
+                    }else if score.teamID == scorecard.visitorteamID{
+                        firstInningsTeamName = scorecard.visitorteam?.name ?? ""
+                    }
+                    firstInningsScore = score.total ?? 0
+                    firstInningsWick = score.wickets ?? 0
+                    firstInningsOver = score.overs ?? 0.0
+                }else if score.scoreboard == "S2"{
+                    if score.teamID == scorecard.localteamID{
+                        secInningsTeamName = scorecard.localteam?.name ?? ""
+                    }else if score.teamID == scorecard.visitorteamID{
+                        secInningsTeamName = scorecard.visitorteam?.name ?? ""
+                    }
+                    secInningsScore = score.total ?? 0
+                    secInningsOver = score.overs ?? 0.0
+                    secInningsWick = score.wickets ?? 0
                 }
             }
             
         }
         //dump(TeamInfo())
-        return TeamInfo(visitorTeam: scorecard.visitorteam?.code ?? "", localTeam: scorecard.localteam?.code ?? "", vistorTeamScore: String(visitorTeamScore) + "-" + String(visitorTeamWick) + "(\(String(visitorTeamOver)))", LocalTeamScore: String(localTeamScore) + "-" + String(localTeamWick) + "(\(String(localTeamOver)))", extrasLocal: localIninigsExtras, extrasVis: visitorIninigsExtras, note: scorecard.note ?? "")
+        return TeamInfo(secInningsTeam: secInningsTeamName, firstInningsTeam: firstInningsTeamName, secInningsScore: String(secInningsScore) + "-" + String(secInningsWick) + "(\(String(secInningsOver)))", firstInningsScore: String(firstInningsScore) + "-" + String(firstInningsWick) + "(\(String(firstInningsOver)))", extrasfirst: firstIninigsExtras, extrasSec: secIninigsExtras, note: scorecard.note ?? "")
     }
-    func setFirstInnigsBat(scorecard: DataClass)-> [BatsmanInfo]?{
-//        guard let batting = scorecard.batting else {return nil}
-//        for batsman in batting {
-////            if batsman.teamID == scorecard.localteam?.id {
-////                BatsmanInfo.firstIningsBatting.append(BatsmanInfo(isExpand: true, batsmanName: <#T##String#>, outStatus: <#T##String#>, run: <#T##String#>, ball: <#T##String#>, four: <#T##String#>, six: <#T##String#>, strikeRate: <#T##String#>))
-////            }
-//            ExpandableInnings.firstIningsBatting[0].batsman.append(<#T##newElement: BatsmanInfo##BatsmanInfo#>)
-//            ExpandableInnings.firstIningsBatting[1].batsman.append(<#T##newElement: BatsmanInfo##BatsmanInfo#>)
-//            ExpandableInnings.firstIningsBatting[3].batsman.append(<#T##newElement: BatsmanInfo##BatsmanInfo#>)
-//        }
+    func setFirstInnigsBat(scorecard: DataClass)-> [ExpandableInnings]?{
+        var batsmanFirst = [BatsmanInfo(batsmanName: "Batsman", outStatus: "", run: "R", ball: "B", four: "4s", six: "6s", strikeRate: "SR")]
+        var batsmanSec = [BatsmanInfo(batsmanName: "Batsman", outStatus: "", run: "R", ball: "B", four: "4s", six: "6s", strikeRate: "SR")]
+        guard let batting = scorecard.batting else {return nil}
+        for batsman in batting {
+            if batsman.scoreboard == "S1" {
+                var outStatus = outStatus(batting: batsman)
+                batsmanFirst.append(BatsmanInfo(batsmanName: batsman.batsman?.firstname ?? "", outStatus: outStatus, run: String(batsman.score ?? 0), ball: String(batsman.ball ?? 0), four: String(batsman.fourX ?? 0), six:String(batsman.sixX ?? 0), strikeRate: String(batsman.rate ?? 0)))
+            }else if batsman.scoreboard == "S2"{
+                var outStatus = outStatus(batting: batsman)
+                batsmanSec.append(BatsmanInfo(batsmanName: batsman.batsman?.firstname ?? "", outStatus: outStatus, run: String(batsman.score ?? 0), ball: String(batsman.ball ?? 0), four: String(batsman.fourX ?? 0), six:String(batsman.sixX ?? 0), strikeRate: String(batsman.rate ?? 0)))
+            }
+            
+        }
+        ExpandableInnings.firstIningsBatting[0].batsman = batsmanFirst
+        ExpandableInnings.firstIningsBatting[1].batsman = batsmanSec
+        return ExpandableInnings.firstIningsBatting
+    }
+    func outStatus(batting: Batting?)-> String{
+        guard let batting = batting  else {return ""}
+        var outStatus = ""
+        if batting.runoutByID == nil && batting.catchStumpPlayerID != nil && batting.bowlingPlayerID == nil{
+            outStatus = (batting.result?.name ?? "") + "(\(batting.catchstump?.firstname ?? ""))"
+        }
+        else if batting.runoutByID != nil {
+            if batting.catchStumpPlayerID != nil{
+                outStatus = (batting.result?.name ?? "") + "(\(batting.catchstump?.firstname ?? "") / \(batting.runoutby?.firstname ?? ""))"
+            }else{
+                outStatus = (batting.result?.name ?? "") + "(\(batting.catchstump?.firstname ?? ""))"
+            }
+        }else if batting.bowlingPlayerID != nil{
+            if batting.catchStumpPlayerID != nil{
+                outStatus = (batting.result?.name ?? "") + " \(batting.catchstump?.firstname ?? "")  B \(batting.bowler?.firstname ?? "")"
+            }else{
+                outStatus = (batting.result?.name ?? "") + "(\(batting.bowler?.firstname ?? ""))"
+            }
+        }else{
+            outStatus = batting.result?.name ?? ""
+        }
+        
+        return outStatus
+    }
+    func setBowling(scorecard: DataClass)-> [ExpandableBowling]?{
+        var bowlerFirst = [BowlerInfo(bowlerName: "Bowler", over: "O", maidenOver: "M", bowlerRun: "R", bowlerWicket: "W", bowlerEconomy: "ER")]
+        var bowlerSec = [BowlerInfo(bowlerName: "Bowler", over: "O", maidenOver: "M", bowlerRun: "R", bowlerWicket: "W", bowlerEconomy: "ER")]
+        guard let bowling = scorecard.bowling else {return nil}
+        for bowler in bowling{
+            if bowler.scoreboard == "S1"{
+                bowlerFirst.append(BowlerInfo(bowlerName: bowler.bowler?.firstname ?? "", over: String(bowler.overs ?? 0), maidenOver: String(bowler.medians ?? 0), bowlerRun: String(bowler.runs ?? 0), bowlerWicket: String(bowler.wickets ?? 0), bowlerEconomy: String(bowler.rate ?? 0)))
+            }else if bowler.scoreboard == "S2"{
+                bowlerSec.append(BowlerInfo(bowlerName: bowler.bowler?.firstname ?? "", over: String(bowler.overs ?? 0), maidenOver: String(bowler.medians ?? 0), bowlerRun: String(bowler.runs ?? 0), bowlerWicket: String(bowler.wickets ?? 0), bowlerEconomy: String(bowler.rate ?? 0)))
+            }
+        }
+        ExpandableBowling.bowlingInfo[0].bowler = bowlerFirst
+        ExpandableBowling.bowlingInfo[1].bowler = bowlerSec
+        return ExpandableBowling.bowlingInfo
     }
 }
 
